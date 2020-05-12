02cfba3e696ea8a089f818294b72f2df5209f94d
sruthivadivoo <41507695+sruthivadivoo@users.noreply.github.com>
2020-05-12 14:49:34 +0530

    Add files via upload

---
+++ mcroserv/src/test/java/com/example/mcroserv/McroservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcroserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McroservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
